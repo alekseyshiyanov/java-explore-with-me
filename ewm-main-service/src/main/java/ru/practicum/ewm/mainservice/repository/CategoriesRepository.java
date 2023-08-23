3543aa0dc4f7064f488bc745b393763008b9f5d6
Aleksey.Shiyanov <shiyanov.aleksey.a@gmail.com>
2023-08-23 13:51:58 +0300

    Внесены изменения

--- ewm-main-service/src/main/java/ru/practicum/ewm/mainservice/repository/CategoriesRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.ewm.mainservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.practicum.ewm.mainservice.model.Categories;
-
-public interface CategoriesRepository extends JpaRepository<Categories, Long> {
-}
