9c282915207278c6c253caa1d8ad1800ab6fbf07
Aleksey.Shiyanov <shiyanov.aleksey.a@gmail.com>
2023-08-23 13:48:12 +0300

    Внесены изменения

--- ewm-main-service/src/main/java/ru/practicum/ewm/EwmMainService.java
+++ ewm-main-service/src/main/java/ru/practicum/ewm/EwmMainService.java
@@ -3,7 +3,7 @@ package ru.practicum.ewm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"ru.practicum.statsclient.client", "ru.practicum.statsclient", "ru.practicum.ewm"})
 public class EwmMainService {
 
     public static void main(String[] args) {
