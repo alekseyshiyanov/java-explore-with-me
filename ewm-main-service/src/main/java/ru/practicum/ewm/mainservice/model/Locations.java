dbb66efd4be88751efbc1aa430c0a2b51bdb897c
Aleksey.Shiyanov <shiyanov.aleksey.a@gmail.com>
2023-08-23 13:26:15 +0300

    Внесены изменения

--- ewm-main-service/src/main/java/ru/practicum/ewm/mainservice/model/Locations.java
+++ ewm-main-service/src/main/java/ru/practicum/ewm/mainservice/model/Locations.java
@@ -2,24 +2,12 @@ package ru.practicum.ewm.mainservice.model;
 
 import lombok.*;
 
-import javax.persistence.*;
-
-@Entity
-@Table(name = "locations", schema = "public")
 @Builder
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Locations {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private Long id;
-
-    @Column(name = "latitude", nullable = false)
-    private Double latitude;
-
-    @Column(name = "longitude", nullable = false)
-    private Double longitude;
+    private Double lat;
+    private Double lon;
 }
